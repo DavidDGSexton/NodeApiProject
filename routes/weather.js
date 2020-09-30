3035e949c5d088ef210c26a5a13b754d8bbeb224
David Sexton <Dsexton@gus.pittstate.edu>
2020-09-29 22:25:30 -0500

    changing api response to use send

--- routes/weather.js
+++ routes/weather.js
@@ -4,9 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('weather', { title: 'Express' });
-
-  const weatherReport = {"Temp": 90.5, "Unit": "fahrenheit", "Cloudy": false, "PercipitationChance": 0};
-  res.write(JSON.stringify(weatherReport));
+  let weatherReport = {"Temp": 90.5, "Unit": "fahrenheit", "Cloudy": false, "PercipitationChance": 0};
+  res.send(weatherReport);
 });
 module.exports = router;
